3cf48e3836908ed1b7c0f9c12fb58dab9727c511
Joseph Anda <orenmurasaki@gmail.com>
2015-07-23 14:40:05 -0700

    refactored more compactly

--- js/app.js
+++ js/app.js
@@ -1,28 +1,32 @@
 var ViewModel = function() {
+
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tommy');
 	this.imgSrc = ko.observable('img/cat1.jpg');
 	this.imgAttribution = ko.observable('www.fakeurl2.org');
-	this.level = ko.observable('Newborn');
-	this.nicknames = ko.observableArray([
-		{ nickname: 'Paws'}, 
-		{ nickname: 'Catherine'}, 
-		{ nickname: 'Scratches'},
-		{ nickname: 'Davies'},
-		{ nickname: 'Stephen'}
-	]);
-
+	this.level = ko.computed(function() {
+		var level;
+		var clicks = this.clickCount();
+		if (clicks < 20) {
+			level = 'Newborn';
+		} else if (clicks < 50) {
+			level = 'Adolescent';
+		} else if (clicks < 100) {
+			level = 'Super Kitty';
+		} else {
+			level = 'Ninja';
+		}
+		return level;
+		}, this);
+	
+	this.nicknames = ko.observableArray([ 'Paws', 'Catherine', 'Scratches', 'Davies', 'Stephen' ]);
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
-
-		if (this.clickCount() >= 20 && this.clickCount() < 50) {
-			this.level('Adolescent');
-		} else if (this.clickCount() >= 50 && this.clickCount() < 100) {
-			this.level('Teen');
-		} else if (this.clickCount() >= 100) {
-			this.level('Super Kitty');
-		}
 	};
 }
+
+
+	
+
 
 ko.applyBindings(new ViewModel());
