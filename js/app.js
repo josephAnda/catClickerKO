818ee247848ba4712098956d8322f81334175da1
Joseph Anda <orenmurasaki@gmail.com>
2015-07-23 14:00:13 -0700

    added nicknames

--- js/app.js
+++ js/app.js
@@ -4,10 +4,17 @@ var ViewModel = function() {
 	this.imgSrc = ko.observable('img/cat1.jpg');
 	this.imgAttribution = ko.observable('www.fakeurl2.org');
 	this.level = ko.observable('Newborn');
+	this.nicknames = ko.observableArray([
+		{ nickname: 'Paws'}, 
+		{ nickname: 'Catherine'}, 
+		{ nickname: 'Scratches'},
+		{ nickname: 'Davies'},
+		{ nickname: 'Stephen'}
+	]);
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
-		
+
 		if (this.clickCount() >= 20 && this.clickCount() < 50) {
 			this.level('Adolescent');
 		} else if (this.clickCount() >= 50 && this.clickCount() < 100) {
