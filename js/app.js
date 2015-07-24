112139bae573f2b813e95556314c0ba8c93f7de1
Joseph Anda <orenmurasaki@gmail.com>
2015-07-24 10:15:04 -0700

    generalized cat creation

--- js/app.js
+++ js/app.js
@@ -1,20 +1,45 @@
-var ViewModel = function() {
-	var self = this;  //  Prevents ambiguity in the event of nested binding contexts and functions
-	self.currentCat = ko.observable( new Cat() );
-	
-	this.incrementCounter = function() {
-		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-	};
-}
-//  The binding contexts referenced in the html determine how observables should be referenced 
-//  In the code.  Furthermore, the meaning of 'this' changes as you nest functions, and the need to 
-//  Reference parents changes as you nest binding contexts.  
-//  Redefining 'this' as 'self' can also help prevent ambiguity with the keyword 'this'
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tommy');
-	this.imgSrc = ko.observable('img/cat1.jpg');
-	this.imgAttribution = ko.observable('www.fakeurl2.org');
+var initialCats = [{  
+	clickCount: 0,
+	name: 'Tommy',
+	imgSrc: 'img/cat1.jpg',
+	imgAttribution: 'http://fakeurl1.com',
+	nicknames: [ 'Paws', 'Catherine', 'Scratches', 'Davies', 'Stephen' ]
+	},
+	{	
+	clickCount: 0,
+	name: 'Laurie',
+	imgSrc: 'img/cat2.jpg',
+	imgAttribution: 'http://fakeurl2.com',
+	nicknames: [ 'Maxi', 'Talim', 'Taki', 'Mitsurugi', 'Cervantes' ]
+	},
+	{
+	clickCount: 0,
+	name: 'Sharon',
+	imgSrc: 'img/cat3.jpg',
+	imgAttribution: 'http://fakeurl3.com',
+	nicknames: [ 'Squall', 'Zell', 'Irvine', 'Selphie', 'Quistis' ]
+	},
+	{
+	clickCount: 0,
+	name: 'Zeus',
+	imgSrc: 'img/cat4.jpg',
+	imgAttribution: 'http://fakeurl4.com',
+	nicknames: [ 'Hera', 'Poseidon', 'Hades', 'Vulcan', 'Athena' ]
+	},
+	{
+	clickCount: 0,
+	name: 'Makoto',
+	imgSrc: 'img/cat5.jpg',
+	imgAttribution: 'http://fakeurl5.com',
+	nicknames: [ 'Zidane', 'Quina', 'Vivi', 'Steiner', 'Garnet' ]
+	}
+]
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
 	this.level = ko.computed(function() {
 		var level;
 		var clicks = this.clickCount();
@@ -29,9 +54,25 @@ var Cat = function() {
 		}
 		return level;
 		}, this);
-	
-	this.nicknames = ko.observableArray([ 'Paws', 'Catherine', 'Scratches', 'Davies', 'Stephen' ]);
 }
+var ViewModel = function() {
+	var self = this;  //  Prevents ambiguity in the event of nested binding contexts and functions
+
+	self.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem){
+		self.catList.push( new Cat(catItem));
+	});
+	self.currentCat = ko.observable( self.catList()[0] );
+	
+	this.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+}
+//  The binding contexts referenced in the html determine how observables should be referenced 
+//  In the code.  Furthermore, the meaning of 'this' changes as you nest functions, and the need to 
+//  Reference parents changes as you nest binding contexts.  
+//  Redefining 'this' as 'self' can also help prevent ambiguity with the keyword 'this'
 
 
 	
